8c31decee58c345be1baeeb1a12d7145d9514060
kiku ishimoto <kikuish@gmail.com>
2020-06-08 00:52:43 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,48 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      album TEXT
+    )
+
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs"
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO dogs (name, breed)
+    VALUES (?,?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+
+    self
+  end
+
+  def self.create(attr_hash)
+    dog = self.new(attr_hash)
+    attr_hash.each do |key, value|
+      dog.send(("#{key}="), value)
+    end
+    dog.save
+  end
+
+
+
+end
